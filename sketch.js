6f6d7d05257075799425c32c1cff98e796a3dbf3
Bia <82560900+BiatrizR@users.noreply.github.com>
2023-01-23 14:38:39 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var balloon,balloonImage1,balloonImage2;
-var database;
+// criar variavel de banco de dados 
+
 var height;
 
 function preload(){
@@ -10,9 +11,9 @@ function preload(){
    "Images/HotAirBallon-02.png","Images/HotAirBallon-03.png","Images/HotAirBallon-03.png","Images/HotAirBallon-03.png");
   }
 
-//Função para definir o ambiente inicial
+
 function setup() {
-  database=firebase.database();
+// Adicionar valor do banco de dados
   createCanvas(1500,700);
 
   balloon=createSprite(250,650,150,150);
@@ -20,11 +21,12 @@ function setup() {
   balloon.scale=0.5;
 
   var balloonHeight=database.ref('balloon/height');
-  balloonHeight.on("value",readHeight, showError);
+   
+// conectar db
   textSize(20); 
 }
 
-// função para exibir a UI
+
 function draw() {
   background(bg);
 
@@ -33,16 +35,16 @@ function draw() {
     balloon.addAnimation("hotAirBalloon",balloonImage2);
   }
   else if(keyDown(RIGHT_ARROW)){
-    updateHeight(10,0);
+    //chamar função de update com direções
     balloon.addAnimation("hotAirBalloon",balloonImage2);
   }
   else if(keyDown(UP_ARROW)){
-    updateHeight(0,-10);
+     //chamar função de update com direções
     balloon.addAnimation("hotAirBalloon",balloonImage2);
     balloon.scale=balloon.scale -0.005;
   }
   else if(keyDown(DOWN_ARROW)){
-    updateHeight(0,+10);
+       //chamar função de update com direções
     balloon.addAnimation("hotAirBalloon",balloonImage2);
     balloon.scale=balloon.scale+0.005;
   }
@@ -56,17 +58,12 @@ function draw() {
 
 
 function updateHeight(x,y){
-  database.ref('balloon/height').set({
-    'x': height.x + x ,
-    'y': height.y + y
-  })
+   //criar referencia dentro do banco de dados
+  
 }
 
 function readHeight(data){
-  height = data.val();
-  console.log(height.x);
-  balloon.x = height.x;
-  balloon.y = height.y;
+ // adicionar valores que bd lerá
 }
 
 function showError(){
